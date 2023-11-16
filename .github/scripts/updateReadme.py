55d0c1852800c44e52080e8d2f9ec9b528296fe4
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-11-17 00:36:25 +0100

    TA Bot hint

--- .github/scripts/updateReadme.py
+++ .github/scripts/updateReadme.py
@@ -42,14 +42,16 @@ if __name__ == "__main__":
             readme_file.write(f"* {total_points} Punkte\n* {timeframe}\n")
             write_horizontal_line(readme_file)
             hint =  "<span style='color:red;'>"
+
             assignmentType=data.get("type","individual")
             if (assignmentType == "group"):
-                hint += ("<b>Info</b>: Bei dieser Aufgabe handelt es sich um eine Gruppenaufgabe, Ihr solltet "
+                hint += ("<b>INFO</b>: Bei dieser Aufgabe handelt es sich um eine Gruppenaufgabe, Ihr solltet "
                         "also miteinander zusammen arbeiten und Euch die Arbeit dabei sinnvoll aufteilen."
-                        "Denkt daran, git fetch/pull bzw. pull --rebase zu benutzen falls Ihr gleichzeitig"
+                        " Denkt daran, git fetch/pull bzw. pull --rebase zu benutzen falls Ihr gleichzeitig"
                         " Veränderungen an denselben Dateien vornehmt, um merge-Konflikte zu reduzieren!<br/>")
             hint += "<b>WICHTIG</b>: Committet/Pusht eure Lösung bitte nicht in den .github-Ordner.<br/> "
             hint += "<b>ACHTUNG</b>: Die Badges unten werden nicht genauso schnell aktualisiert wie die Pipeline (Actions)<br/>" 
+            hint += "<b>Tipp</b>: Der TA-Bot kann Euch am Besten helfen, wenn Ihr regelmäßige kleine Commits pusht-um einen Blick auf Euren Stand zu pushen (es ist völlig normal, alle 5 Minuten zu pushen)."
             hint += "Für Übersetzungen der Aufgabe in eine andere Sprache: https://translate.google.de <br/>"
             hint += "</span>"
             readme_file.write(f"<p>{hint} </p>\n")
